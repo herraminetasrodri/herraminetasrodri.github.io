1c9be7d6f98ea65eeaeaf8f826c7df8f6d5dfb9f
herraminetasrodri <118705809+herraminetasrodri@users.noreply.github.com>
2022-11-20 21:41:00 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "herraientasmhr.firebaseapp.com",
-    projectId: "herraientasmhr",
-    storageBucket: "herraientasmhr.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyCKwJJblnjnjWEa-0Rmxz45GUotdOL1W3Y",
+  authDomain: "herraminetasrodri1.firebaseapp.com",
+  projectId: "herraminetasrodri1",
+  storageBucket: "herraminetasrodri1.appspot.com",
+  messagingSenderId: "394116529265",
+  appId: "1:394116529265:web:2f4890972016eceaabafd0",
+  measurementId: "G-CMVX9XZLC8"
 });
